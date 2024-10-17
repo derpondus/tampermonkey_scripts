27eae963a24d15808b7389c7b2d2fdd457b66b98
derpondus <lasse.liening-ewert@t-online.de>
2024-10-17 03:48:05 +0200

    probably final body-regex fix

--- src/campuspoint_scraper.user.js
+++ src/campuspoint_scraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CampusPoint extractor
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  pack listing data from campuspoint.de into a csv in the console
 // @author       PondusDev
 // @match        https://www.campuspoint.de/mobile/notebooks*
@@ -43,7 +43,7 @@
 
         const title_regex = /.*?(Lenovo|HP)(?: Campus)?\s(?:([^()G]*)\s)?([^G()][^()\s]*)\s(?:(G\S*)\s)?(.*)/
         const artnr_regex = /.*?:\s(.*)/
-        const body_regex = /.*?\s\((\S+")\)\s(.*?)\s\((.+\s?x\s?.+?)(?:,.*)?\).*?,\s(Intel.*|AMD.*|Qualcomm.*|Snapdragon.*),\s(.*?),\s(.*?),\s.*?(Intel.*?|AMD.*?|NVIDIA.*?|Qualcomm.*?),\s(.*)/
+        const body_regex = /.*?\s\((\S+")\)\s(.*?)\s\((.+\s?x\s?.+?)(?:,.*)?\).*?,\s.*?(Intel.*|AMD.*|Qualcomm.*|Snapdragon.*),\s(.*?),\s(.*?),\s.*?(Intel.*?|AMD.*?|NVIDIA.*?|Qualcomm.*?),\s(.*)/
 
         const product_holder = document.querySelectorAll(base_selector)
         const data = []
