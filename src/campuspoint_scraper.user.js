656a018e72f84c7fea29c61ef0d74f8dbb0bcafc
derpondus <lasse.liening-ewert@t-online.de>
2024-10-17 03:34:23 +0200

    add scrolling up after going down too far

--- src/campuspoint_scraper.user.js
+++ src/campuspoint_scraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CampusPoint extractor
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  pack listing data from campuspoint.de into a csv in the console
 // @author       PondusDev
 // @match        https://www.campuspoint.de/mobile/notebooks*
@@ -114,7 +114,10 @@
         console.log(convertToCSV(Object.values(all_data)))
 
         const toolbar_bottom = document.querySelector(".toolbar-bottom");
-        if (toolbar_bottom !== null) toolbar_bottom.scrollIntoView({behavior: "smooth"})
+        if (toolbar_bottom !== null) {
+            toolbar_bottom.scrollIntoView({behavior: "smooth"})
+            window.scrollBy(0, -500)
+        }
     }
 
     const button = document.createElement('button')
