ea827e5d5767f228c15ec21670e258615032801d
derpondus <lasse.liening-ewert@t-online.de>
2024-10-18 15:06:24 +0200

    create cpubenchmark_scraper.user.js

---
+++ src/cpubenchmark_scraper.user.js
@@ -0,0 +1,85 @@
+// ==UserScript==
+// @name         CPUBenchmark extractor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  pack benchmark data from cpubenchmark.net into a csv in the console
+// @author       PondusDev
+// @match        https://www.cpubenchmark.net/CPU_mega_page.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cpubenchmark.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const all_data = {}
+
+    // Your code here...
+    function exec() {
+        function convertToCSV(arr) {
+            const columns=["artnr", "brand", "name", "version", "gen", "oldPrice", "price", "disp_diagonal", "disp_type", "disp_size", "cpu", "gpu", "os", "ram", "ssd", "lte", "form", "title_ext", "body_ext"]
+            const rows = [columns.join("\t")]
+            for (const value of arr) {
+                rows.push(columns.map(it => value[it]).join("\t"))
+            }
+            return rows.join("\n")
+        }
+        // -----
+
+        const column_selector = "#cputable > thead > tr > th:not(:first-child)"
+        const columns = Array.from(document.querySelectorAll(column_selector)).map(it => it.innerText
+            .trim().toLowerCase().replace(/\s/g, "_").replace(/[^a-z_]/g, ""))
+
+        const base_selector = "#cputable > tbody > tr"
+
+        const value_selector = function(column) {
+            return `td:nth-of-type(${columns.indexOf(column) + 2})`
+        }
+
+        const item_holder = document.querySelectorAll(base_selector)
+        const data = []
+
+        for (const item of item_holder) {
+            const next_data = {};
+
+            for (const column of columns) {
+                next_data[column] = item.querySelector(value_selector(column)).innerText.trim()
+            }
+
+            data.push(next_data)
+        }
+
+        //console.log(data)
+        //console.log(convertToCSV(data))
+
+        for (const item of data) {
+            if (item[columns[0]] in all_data) {
+                console.log("--- Duplicate ---")
+                console.log(item)
+                console.log(item[columns[0]])
+                let diff = false
+                for (const key of Object.keys(product)) {
+                    if (item[key] !== all_data[item[columns[0]]][key]) {
+                        console.log(`${key}: ${item[key]} !== ${all_data[item[columns[0]]][key]}`)
+                        diff = true
+                    }
+                }
+                if (!diff) console.log(">>>>> No difference <<<<<")
+                console.log("----- -----")
+            }
+            all_data[item[columns[0]]] = item
+        }
+
+        console.log(Object.values(all_data))
+        console.log(convertToCSV(Object.values(all_data)))
+    }
+
+    const button = document.createElement('button')
+    button.style.position = 'absolute'
+    button.style.top = '10px'
+    button.style.left = '10px'
+    button.style.zIndex = '1000'
+    button.addEventListener('click', exec)
+    button.innerText = 'Click me!'
+    document.body.appendChild(button)
+})();
